4b4e78c54ea798164590ab34bf90f0a2570f4f17
Craft <william.craft@accenture.com>
2021-05-21 15:54:18 -0400

    for loop loops through simple array of RE characters.

---
+++ arraysExample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace arraysProgram
+{
+    class arraysExample
+    {
+        static void Main(string[] args)
+        {
+            string[] RECharacters = {"Chris", "Leon", "Jill", "Claire", "Ada", "Barry", "Wesker", "Ethan", "Rebecca"};
+            string[] fruitArray = {};
+
+            Console.WriteLine("Here are all the main characters from Resident Evil");
+            for(int i = 0; i< RECharacters.Length; i++){
+                Console.WriteLine(RECharacters[i]);
+            }
+
+            Console.WriteLine("Thanks, press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
