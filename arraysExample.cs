9d688235af461085ba73458821e8b9b3ad817679
Craft <william.craft@accenture.com>
2021-05-21 16:28:25 -0400

    added an empty array for the user to fill

--- arraysExample.cs
+++ arraysExample.cs
@@ -7,11 +7,24 @@ namespace arraysProgram
         static void Main(string[] args)
         {
             string[] RECharacters = {"Chris", "Leon", "Jill", "Claire", "Ada", "Barry", "Wesker", "Ethan", "Rebecca"};
-            string[] fruitArray = {};
+            string[] fruitArray = new string[5];
 
             Console.WriteLine("Here are all the main characters from Resident Evil");
-            for(int i = 0; i< RECharacters.Length; i++){
+            for(int i = 0; i < RECharacters.Length; i++){
                 Console.WriteLine(RECharacters[i]);
+            }
+
+            Console.WriteLine("Now, enter in your favorite fruits:");
+            for(int i = 0; i < fruitArray.Length; i++){
+                fruitArray[i] = Console.ReadLine();
+            }
+
+            Array.Sort(fruitArray);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Here are the fruits you entered:");
+            for(int i = 0; i < fruitArray.LongLength; i++){
+                Console.WriteLine(fruitArray[i]);
             }
 
             Console.WriteLine("Thanks, press any key to exit.");
